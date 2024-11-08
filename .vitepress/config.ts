1609ce4a4fe9e576da85f08db468bf967e879fa8
FrodBRA <felix.frodermann@br.nrw.de>
2024-11-08 15:26:07 +0100

    Weitere Über-Kategorien in "collapsed: false" gekapselt; "Client" testweise mit ** versehen, um Fettdruck zu erzeugen

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -117,7 +117,7 @@ export default defineConfig(({ mode }) => {
 				'/client': [
 					{ text: '', items:
 						[
-							{ text: '⟿ Der Client ⇜', link: '/client/client/', collapsed: false, items: [
+							{ text: '**⟿ Der Client ⇜**', link: '/client/client/', collapsed: false, items: [
 								{ text: 'Tastaturnavigation', link: '/client/client/tastaturnavigation' },
 								{ text: 'Änderungen', link: '/client/client/änderungen/' },
 								{ text: 'FAQ', link: '/client/client/faq/' },
@@ -133,85 +133,87 @@ export default defineConfig(({ mode }) => {
 								] },
 								{ text: 'Zeugnisvorbereitung', link: '/client/anleitungen_zeugnisse/' }
 							] },
-							{ text: '⟿ Apps ⇜', link: '/client/apps/' },
-							{ text: 'Aktueller Benutzer', link: '/client/aktuellernutzer/' },
-							{ text: 'Schule', link: '/client/schule/', collapsed: true, items: [
-								{ text: 'Schulbezogene Kataloge', collapsed: true, items: [
-									{ text: 'Stammdaten der Schule', link: '/client/schule/' },
-									{ text: 'Betriebe (Todo)', link: '/client/schule/kataloge/sb_betriebe/' },
-									{ text: 'Einwilligungsarten (Todo)', link: '/client/schule/kataloge/sb_einwilligungsarten/' },
-									{ text: 'Fächer', link: '/client/schule/kataloge/sb_faecher/' },
-									{ text: 'Förderschwerpunkte', link: '/client/schule/kataloge/sb_foerderschwerpunkte/' },
-									{ text: 'Jahrgänge', link: '/client/schule/kataloge/sb_jahrgaenge/' },
-									{ text: 'Vermerkarten (todo)', link: '/client/schule/kataloge/sb_vermerkarten/' }
+							{ text: '⟿ Apps ⇜', link: '/client/apps/', collapsed: false, items: [
+								{ text: 'Aktueller Benutzer', link: '/client/aktuellernutzer/' },
+								{ text: 'Schule', link: '/client/schule/', collapsed: true, items: [
+									{ text: 'Schulbezogene Kataloge', collapsed: true, items: [
+										{ text: 'Stammdaten der Schule', link: '/client/schule/' },
+										{ text: 'Betriebe (Todo)', link: '/client/schule/kataloge/sb_betriebe/' },
+										{ text: 'Einwilligungsarten (Todo)', link: '/client/schule/kataloge/sb_einwilligungsarten/' },
+										{ text: 'Fächer', link: '/client/schule/kataloge/sb_faecher/' },
+										{ text: 'Förderschwerpunkte', link: '/client/schule/kataloge/sb_foerderschwerpunkte/' },
+										{ text: 'Jahrgänge', link: '/client/schule/kataloge/sb_jahrgaenge/' },
+										{ text: 'Vermerkarten (todo)', link: '/client/schule/kataloge/sb_vermerkarten/' }
+									] },
+									{ text: 'Allgemeine Kataloge', collapsed: true, items: [
+										{ text: 'Religionen', link: '/client/schule/kataloge/religionen/' },
+										{ text: 'Schulen (Todo)' }
+									] },
+									{ text: 'Datenaustausch', link: '/client/schule/datenaustausch/', collapsed: true, items: [
+										{ text: 'ENM Notenmanager (Todo)' },
+										{ text: 'LuPO Laufbahnplanung', link: '/client/schule/datenaustausch/lupo/' },
+										{ text: 'WebNotenManager', link: '/client/schule/datenaustausch/wenom/' },
+										{ text: 'Kurs 42 (Todo)' },
+										{ text: 'Untis (Todo)' }
+									] }
 								] },
-								{ text: 'Allgemeine Kataloge', collapsed: true, items: [
-									{ text: 'Religionen', link: '/client/schule/kataloge/religionen/' },
-									{ text: 'Schulen (Todo)' }
+								{ text: 'Schüler (Anpassen!)', link: '/client/schueler/', collapsed: true, items: [
+									{ text: 'Individualdaten', link: '/client/schueler/individualdaten/' },
+									{ text: 'Erziehungsberechtigte', link: '/client/schueler/erziehungsberechtigte/' },
+									{ text: 'Ausbildungsbetriebe', link: '/client/schueler/ausbildungsbetriebe/' },
+									{ text: 'Schulbesuch', link: '/client/schueler/schulbesuch/' },
+									{ text: 'Lernabschnitte', link: '/client/schueler/lernabschnitte/', collapsed: true, items: [
+										{ text: 'Allgemein', link: '/client/schueler/lernabschnitte/allgemein/' },
+										{ text: 'Leistungsdaten', link: '/client/schueler/lernabschnitte/leistungsdaten/' }
+									] },
+									{ text: 'KAoA', link: '/client/schueler/kaoa/' },
+									{ text: 'Laufbahn', link: '/client/schueler/laufbahn/' },
+									{ text: 'Laufbahnplanung Sek II', link: '/client/schueler/laufbahnplanung/' },
+									{ text: 'Stundenplan', link: '/client/schueler/stundenplan/' }
 								] },
-								{ text: 'Datenaustausch', link: '/client/schule/datenaustausch/', collapsed: true, items: [
-									{ text: 'ENM Notenmanager (Todo)' },
-									{ text: 'LuPO Laufbahnplanung', link: '/client/schule/datenaustausch/lupo/' },
-									{ text: 'WebNotenManager', link: '/client/schule/datenaustausch/wenom/' },
-									{ text: 'Kurs 42 (Todo)' },
-									{ text: 'Untis (Todo)' }
-								] }
-							] },
-							{ text: 'Schüler (Anpassen!)', link: '/client/schueler/', collapsed: true, items: [
-								{ text: 'Individualdaten', link: '/client/schueler/individualdaten/' },
-								{ text: 'Erziehungsberechtigte', link: '/client/schueler/erziehungsberechtigte/' },
-								{ text: 'Ausbildungsbetriebe', link: '/client/schueler/ausbildungsbetriebe/' },
-								{ text: 'Schulbesuch', link: '/client/schueler/schulbesuch/' },
-								{ text: 'Lernabschnitte', link: '/client/schueler/lernabschnitte/', collapsed: true, items: [
-									{ text: 'Allgemein', link: '/client/schueler/lernabschnitte/allgemein/' },
-									{ text: 'Leistungsdaten', link: '/client/schueler/lernabschnitte/leistungsdaten/' }
+								{ text: 'Lehrkräfte', link: '/client/lehrer/', collapsed: true, items: [
+									{ text: 'Individualdaten', link: '/client/lehrer/individualdaten/' },
+									{ text: 'Unterricht', link: '/client/lehrer/unterricht/' },
+									{ text: 'Stundenplan', link: '/client/lehrer/stundenplan/' }
 								] },
-								{ text: 'KAoA', link: '/client/schueler/kaoa/' },
-								{ text: 'Laufbahn', link: '/client/schueler/laufbahn/' },
-								{ text: 'Laufbahnplanung Sek II', link: '/client/schueler/laufbahnplanung/' },
-								{ text: 'Stundenplan', link: '/client/schueler/stundenplan/' }
-							] },
-							{ text: 'Lehrkräfte', link: '/client/lehrer/', collapsed: true, items: [
-								{ text: 'Individualdaten', link: '/client/lehrer/individualdaten/' },
-								{ text: 'Unterricht', link: '/client/lehrer/unterricht/' },
-								{ text: 'Stundenplan', link: '/client/lehrer/stundenplan/' }
-							] },
-							{ text: 'Klassen', link: '/client/klassen/' },
-							{ text: 'Kurse', link: '/client/kurse/' },
-							{
-								text: 'Oberstufe', link: '/client/gost/', collapsed: true, items: [
-									{ text: 'Abiturjahrgang', link: '/client/gost/abiturjahrgang/' },
-									{ text: 'Fächer', link: '/client/gost/faecher/' },
-									{ text: 'Beratung', link: '/client/gost/beratung/' },
-									{ text: 'Laufbahnplanung', link: '/client/gost/laufbahn/' },
-									{ text: 'Fachwahlen', link: '/client/gost/fachwahlen/' },
-									{ text: 'Kursplanung', link: '/client/gost/kursplanung/' },
-									{ text: 'Klausurplanung', link: '/client/gost/klausurplanung/' }
+								{ text: 'Klassen', link: '/client/klassen/' },
+								{ text: 'Kurse', link: '/client/kurse/' },
+								{
+									text: 'Oberstufe', link: '/client/gost/', collapsed: true, items: [
+										{ text: 'Abiturjahrgang', link: '/client/gost/abiturjahrgang/' },
+										{ text: 'Fächer', link: '/client/gost/faecher/' },
+										{ text: 'Beratung', link: '/client/gost/beratung/' },
+										{ text: 'Laufbahnplanung', link: '/client/gost/laufbahn/' },
+										{ text: 'Fachwahlen', link: '/client/gost/fachwahlen/' },
+										{ text: 'Kursplanung', link: '/client/gost/kursplanung/' },
+										{ text: 'Klausurplanung', link: '/client/gost/klausurplanung/' }
+									] },
+								{ text: 'Statistik', link: '/client/statistik/', collapsed: true, items: [
+									{ text: 'Verschlüsselung ', link: '/client/statistik/verschluesselung/' },
+									{ text: 'Datenprüfung', link: '/client/statistik/datenpruefung/' },
+									{ text: 'Dokumente', link: '/client/statistik/dokumente/' },
+									{ text: 'Hilfe', link: '/client/statistik/hilfe/' }
 								] },
-							{ text: 'Statistik', link: '/client/statistik/', collapsed: true, items: [
-								{ text: 'Verschlüsselung ', link: '/client/statistik/verschluesselung/' },
-								{ text: 'Datenprüfung', link: '/client/statistik/datenpruefung/' },
-								{ text: 'Dokumente', link: '/client/statistik/dokumente/' },
-								{ text: 'Hilfe', link: '/client/statistik/hilfe/' }
+								{ text: 'Stundenplan', link: '/client/stundenplan/', collapsed: true, items: [
+									{ text: 'Grundlagen zum Stundenplan', link: '/client/stundenplan/stundenplan_basisinformationen/' },
+									{ text: 'Hierhier: Stundenplananleitung?' },
+									{ text: 'Allgemeine Vorlagen', link: '/client/stundenplan/allgemeine_vorlagen/', collapsed: true, items: [
+										{ text: 'Aufsichtsbereiche', link: '/client/stundenplan/allgemeine_vorlagen/aufsichtsbereiche/' },
+										{ text: 'Pausenzeiten', link: '/client/stundenplan/allgemeine_vorlagen/pausenzeiten/' },
+										{ text: 'Räume', link: '/client/stundenplan/allgemeine_vorlagen/raeume/' },
+										{ text: 'Zeitraster', link: '/client/stundenplan/allgemeine_vorlagen/zeitraster/' }
+									] }
+								] },
+								{ text: 'Einstellungen', link: '/client/einstellungen/', collapsed: true, items: [
+									{ text: 'Benutzer (Anzupassen)', link: '/client/einstellungen/benutzer/' },
+									{ text: 'Benutzergruppen (Anzupassen)', link: '/client/einstellungen/benutzergruppen/' }
+								] },
 							] },
-							{ text: 'Stundenplan', link: '/client/stundenplan/', collapsed: true, items: [
-								{ text: 'Grundlagen zum Stundenplan', link: '/client/stundenplan/stundenplan_basisinformationen/' },
-								{ text: 'Hierhier: Stundenplananleitung?' },
-								{ text: 'Allgemeine Vorlagen', link: '/client/stundenplan/allgemeine_vorlagen/', collapsed: true, items: [
-									{ text: 'Aufsichtsbereiche', link: '/client/stundenplan/allgemeine_vorlagen/aufsichtsbereiche/' },
-									{ text: 'Pausenzeiten', link: '/client/stundenplan/allgemeine_vorlagen/pausenzeiten/' },
-									{ text: 'Räume', link: '/client/stundenplan/allgemeine_vorlagen/raeume/' },
-									{ text: 'Zeitraster', link: '/client/stundenplan/allgemeine_vorlagen/zeitraster/' }
-								] }
+							{ text: '⟿ Weiteres ⇜', link: '/client/weiteres/', collapsed: false, items: [
+								{ text: 'JSON-Dateien', link: '/client/client/json_files/' },
+								{ text: 'Adressbücher', link: '/client/adressbuecher/' },
+								{ text: 'Kalender', link: '/client/kalender/' }
 							] },
-							{ text: 'Einstellungen', link: '/client/einstellungen/', collapsed: true, items: [
-								{ text: 'Benutzer (Anzupassen)', link: '/client/einstellungen/benutzer/' },
-								{ text: 'Benutzergruppen (Anzupassen)', link: '/client/einstellungen/benutzergruppen/' }
-							] },
-							{ text: '⟿ Weiteres ⇜', link: '/client/weiteres/' },
-							{ text: 'JSON-Dateien', link: '/client/client/json_files/' },
-							{ text: 'Adressbücher', link: '/client/adressbuecher/' },
-							{ text: 'Kalender', link: '/client/kalender/' },
 							{ text: '―――'},
 							{ text: 'Writing Guidelines (Temp)', link: '/client/writingguide/' }
 						]
